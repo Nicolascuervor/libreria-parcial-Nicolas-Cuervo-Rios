690f267efa402b7cdbf27b6dadeecd9483f5fd14
Nicolás <ncuervo_175@cue.edu.co>
2026-05-22 18:32:59 -0500

    🟢 GREEN: Implementa Regla 1 (Precio base > 0)

--- src/producto.py
+++ src/producto.py
@@ -1,4 +1,6 @@
 class Producto:
     def __init__(self, nombre: str, precio_base: float):
-        self.nombre = None
-        self.precio_base = 0
+        if precio_base <= 0:
+            raise ValueError("El precio base debe ser mayor que cero")
+        self.nombre = nombre
+        self.precio_base = precio_base
